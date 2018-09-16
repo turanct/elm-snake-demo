5504ce25cad0b3d72ca058f27846645125d36ef2
turanct <spinnewebber_toon@hotmail.com>
2018-09-16 23:26:22 +0200

    Move to Elm 0.19 and use keyboard to update Direction

--- Main.elm
+++ Main.elm
@@ -1,12 +1,14 @@
+import Browser
 import Html exposing (Html, div)
-import Html.Attributes exposing (class, placeholder, href)
-import Html.Events exposing (onInput, onClick, onSubmit)
-import List exposing (head, take, length, range, concat, map, member)
-import Time exposing (Time, every, millisecond)
+import Html.Attributes exposing (class)
+import List exposing (head, take, length, range, concat, map, member, reverse, drop)
+import Time exposing (every)
+import Json.Decode as Decode
+import Browser.Events exposing (onKeyDown)
 
 
 main =
-  Html.program
+  Browser.element
     { init = init
     , view = view
     , update = update
@@ -33,10 +35,10 @@ type alias Model =
   , direction: Direction
   }
 
-init : (Model, Cmd Msg)
-init =
+init : () -> (Model, Cmd Msg)
+init _ =
   ( { snake = [ { x = 0, y = 0}, { x = 1, y = 0}, {x = 2, y = 0}, {x = 3, y = 0} ]
-    , bait = {x = 5, y = -5}
+    , bait = {x = 5, y = 5}
     , direction = Left
     }
   , Cmd.none
@@ -46,13 +48,20 @@ init =
 -- UPDATE
 
 type Msg
-  = Tick Time
+  = Tick Time.Posix
+  | Change Direction
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  ( move model
-  , Cmd.none
-  )
+  case msg of
+    Tick _ ->
+      ( move model
+      , Cmd.none
+      )
+    Change direction ->
+      ( { model | direction = direction }
+      , Cmd.none
+      )
 
 move : Model -> Model
 move model =
@@ -65,9 +74,9 @@ move model =
     newHead =
       case model.direction of
         Up ->
-          { oldHead | y = oldHead.y + 1 }
-        Down ->
           { oldHead | y = oldHead.y - 1 }
+        Down ->
+          { oldHead | y = oldHead.y + 1 }
         Right ->
           { oldHead | x = oldHead.x + 1 }
         Left ->
@@ -75,13 +84,32 @@ move model =
   in
   { model | snake = newHead :: take (length model.snake - 1) model.snake}
 
+
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  div [ class "wrapper"]
-    [
-    ]
+  div [ class "wrapper" ] (drawGrid model)
+
+drawGrid : Model -> List (Html Msg)
+drawGrid model =
+  let
+    fields =
+      map
+        (\(coord, bool) ->
+          div [ class (if bool == True then "snake" else "field") ] [])
+        (renderGrid model.snake model.bait)
+    drawLines = map (\l -> div [ class "line"] l)
+  in
+  fields |> lines |> drawLines
+
+lines fs = lines2 [] fs
+
+lines2 =
+  \l f ->
+    case f of
+      [] -> reverse l
+      _ -> lines2 ((take 20 f) :: l) (drop 20 f)
 
 renderGrid : Snake -> Bait -> List (Coordinate, Bool)
 renderGrid snake bait =
@@ -91,7 +119,7 @@ renderGrid snake bait =
         filled = (member c snake) || c == bait
       in
       (c, filled))
-    (emptyGrid -5 5)
+    (emptyGrid 0 19)
 
 emptyGrid : Int -> Int -> List Coordinate
 emptyGrid min max =
@@ -106,5 +134,25 @@ emptyGrid min max =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  every (333 * millisecond) Tick
+  Sub.batch
+    [ every 150 Tick
+    , onKeyDown keyDecoder
+    ]
 
+keyDecoder : Decode.Decoder Msg
+keyDecoder =
+  Decode.map toDirection (Decode.field "key" Decode.string)
+
+toDirection : String -> Msg
+toDirection string =
+  case string of
+    "ArrowUp" ->
+      Change Up
+    "ArrowDown" ->
+      Change Down
+    "ArrowRight" ->
+      Change Right
+    "ArrowLeft" ->
+      Change Left
+    _ ->
+      Change Left
--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ compile: \
 	main.js
 
 main.js: Main.elm
-	elm-make Main.elm --output main.js
+	elm make Main.elm --output main.js
 
 .PHONY: compile
